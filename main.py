adb23749d0be5c3abf5af048b51086b70499771b
MinhThien <nguyenanminhthien97@gmail.com>
2025-02-18 19:47:38 +0700

    Add new column Linkedin at file main.py

--- main.py
+++ main.py
@@ -14,11 +14,24 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import argparse
 
-def extract_page(data_url, rows_number, driver):
+
+def extract_page(data_url, rows_number, driver,add_linkedin):
     info_list = list()
     driver.get(data_url)
     WebDriverWait(driver,10).until(expected_conditions.presence_of_element_located((By.XPATH, '//div[contains(@role,"treegrid")]')))
     time.sleep(10)
+
+    # Add linkedin as requirement.
+    if add_linkedin == True:
+        driver.find_element(By.CLASS_NAME,'apollo-icon-plus').click()
+        ActionChains(driver) \
+            .send_keys("Links") \
+            .send_keys(Keys.ENTER) \
+            .perform()
+        ActionBuilder(driver).clear_actions()
+        ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+    time.sleep(3)
 
     treegrid = driver.find_element(By.XPATH, '//div[contains(@role,"treegrid")]')
     rows = treegrid.find_elements(By.XPATH, '//div[contains(@role,"row")]')
@@ -30,56 +43,62 @@ def extract_page(data_url, rows_number, driver):
         sub_list.append(sub[0])
         sub_list.append(sub[1])
         sub_list.append(sub[2])
-        sub_list.append(sub[5])
-        sub_list.append(sub[6])
-        if sub[1].__contains__("Update available"):
-            print("break")
-            sub_list[1] = sub[2]
-            sub_list[2] = sub[4]
-            sub_list[3] = sub[8]
-            sub_list[4] = sub[9]
+
         # email-5
         try:
             email_button = rows[i].find_element(By.CLASS_NAME, "apollo-colored-icon")
-            ActionChains(driver) \
-                .move_to_element(email_button) \
-                .click() \
-                .perform()
-            ActionBuilder(driver).clear_actions()
+            parrent_email = email_button.find_element(By.XPATH,"./..")
+            # ActionChains(driver) \
+            #     .move_to_element(parrent_email) \
+            #     .click() \
+            #     .perform()
+            # ActionBuilder(driver).clear_actions()
+            driver.execute_script("arguments[0].click();", parrent_email)
             # time.sleep(2)
             email = driver.find_element(By.XPATH, '//div[@data-testid="ContactInformationContainer"]')
             sub_list.append(email.text.split("\n")[0])
         except selenium.common.exceptions.NoSuchElementException as e:
             sub_list.append("N/A")
 
+        # Add linkedin
+        linkedin = rows[i].find_element(By.CLASS_NAME, 'apollo-icon-linkedin')
+        parrent = linkedin.find_element(By.XPATH, "./..")
+        sub_list.append(parrent.get_attribute("href"))
+
+        # Status
+        sub_list.append("")
+
+        sub_list.append(sub[5])
+        sub_list.append(sub[6])
+        if sub[1].__contains__("Update available"):
+            print("break")
+            sub_list[1] = sub[2]
+            sub_list[2] = sub[4]
+            sub_list[6] = sub[8]
+            sub_list[7] = sub[9]
         # phone-6
         ActionChains(driver).send_keys(Keys.ESCAPE).perform()
         phone_button =  rows[i].find_element(By.CLASS_NAME, "apollo-icon-phone")
+        parrent_phone = phone_button.find_element(By.XPATH, "./..")
         try:
-            ActionChains(driver) \
-                .move_to_element(phone_button) \
-                .click() \
-                .perform()
-            ActionBuilder(driver).clear_actions()
+            # ActionChains(driver) \
+            #     .move_to_element(parrent_phone) \
+            #     .click() \
+            #     .perform()
+            # ActionBuilder(driver).clear_actions()
+            driver.execute_script("arguments[0].click();", parrent_phone)
             # time.sleep(2)
             phone_number = driver.find_element(By.XPATH, '//div[@data-testid="ContactPhoneInformationContainer"]')
             phone_number.find_element(By.TAG_NAME, 'label').click()
             sub_list.append(phone_number.text.split("\n")[1])
         except Exception as e:
             sub_list.append("N/A")
-        sub_list.append(rows[i].text.split(sub_list[4])[1].strip("\n"))
+        #     Remove company keywords.
+        # sub_list.append(rows[i].text.split(sub_list[4])[1].strip("\n"))
+        ActionChains(driver).send_keys(Keys.ESCAPE).perform()
         info_list.append(sub_list)
     return info_list
 
-def move_next_range(driver, select):
-    ActionChains(driver) \
-        .move_to_element(select) \
-        .click_and_hold() \
-        .key_down(Keys.ARROW_DOWN) \
-        .key_down(Keys.ENTER) \
-        .pause(2) \
-        .perform()
-    ActionBuilder(driver).clear_actions()
 ## Create a session objec
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Web scraping")
@@ -90,6 +109,7 @@ if __name__ == "__main__":
     parser.add_argument("-records", help="Total records contain in that webpage", required=True)
 
     args = parser.parse_args()
+    start_time = time.time()
     # calculate total number
     total_number = int(args.records)
     rows_number = 25
@@ -117,15 +137,21 @@ if __name__ == "__main__":
     final_list = list()
     count = 0
     for rows_number in my_array:
-        info_list = extract_page(data_url=data_url,rows_number=rows_number,driver=driver)
+        if count == 0:
+            flag = True
+        else:
+            flag = False
+        info_list = extract_page(data_url=data_url,rows_number=rows_number,driver=driver, add_linkedin=flag)
         time.sleep(time_random)
         final_list = final_list + info_list
         count = count + 1
         if count < my_array.__len__():
-            driver.find_element(By.CLASS_NAME, 'apollo-icon-chevron-arrow-right').click()
+            go_to_next = driver.find_element(By.CLASS_NAME, 'apollo-icon-chevron-arrow-right')
+            driver.execute_script("arguments[0].click();", go_to_next)
 
     driver.close()
-    pandas.DataFrame(final_list, columns=['Name','Job Title', 'Company','Location', 'Company','Email','Phone','Company industries and Keywords']).to_csv(f"{args.output}",index=False)
+    pandas.DataFrame(final_list, columns=['Name','Job Title', 'Company','Email', 'URL Linkedin', 'Status','Location', 'Company Number','Phone']).to_csv(f"{args.output}",index=False)
+    print(f"Time taken: {time.time() - start_time}\n")
 
 
     print("Output file here: \n")
